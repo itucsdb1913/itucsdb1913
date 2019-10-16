fa115e3d75ccfe8f468f0c93c2cd407ee0edd640
karayilano <karayilano@itu.edu.tr>
2019-10-16 16:21:35 +0300

    flask test

--- server.py
+++ server.py
@@ -1,53 +1,29 @@
 from flask import Flask
-from flask_login import LoginManager
-
 import views
 from database import Database
-#from movie import Movie
-from user import get_user
+from movie import Movie
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-lm = LoginManager()
+    app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/movies", view_func=views.movies_page, methods=["GET", "POST"])
+    app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
+    app.add_url_rule("/new-movie", view_func=views.movie_add_page, methods=["GET", "POST"])
+    app.add_url_rule(
+        "/movies/<int:movie_key>/edit",
+        view_func=views.movie_edit_page,
+        methods=["GET", "POST"],
+    )
 
+    db = Database()
+    db.add_movie(Movie("Slaughterhouse-Five", year=1972))
+    db.add_movie(Movie("The Shining"))
+    app.config["db"] = db
 
-@lm.user_loader
-def load_user(user_id):
-    return get_user(user_id)
-
-
-#def create_app():
-app.config.from_object("settings")
-
-app.add_url_rule("/", view_func=views.home_page, methods=["GET", "POST"])
-
-    # app.add_url_rule(
-    #     "/login", view_func=views.login_page, methods=["GET", "POST"]
-    # )
-    # app.add_url_rule("/logout", view_func=views.logout_page)
-    #
-    # app.add_url_rule(
-    #     "/movies", view_func=views.movies_page, methods=["GET", "POST"]
-    # )
-    # app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
-    # app.add_url_rule(
-    #     "/movies/<int:movie_key>/edit",
-    #     view_func=views.movie_edit_page,
-    #     methods=["GET", "POST"],
-    # )
-    # app.add_url_rule(
-    #     "/new-movie", view_func=views.movie_add_page, methods=["GET", "POST"]
-    # )
-
-    # lm.init_app(app)
-    # lm.login_view = "login_page"
-
-db = Database()
-app.config["db"] = db
-
-    #return app
+    return app
 
 
 if __name__ == "__main__":
-    #create_app()
+    app = create_app()
     app.run()
