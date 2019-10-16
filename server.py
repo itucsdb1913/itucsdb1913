b61560d3508eb4d25f36c77b0c939eb09146767a
karayilano <karayilano@itu.edu.tr>
2019-10-16 12:43:28 +0300

    Main page only

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager
 
 import views
 from database import Database
-from movie import Movie
+#from movie import Movie
 from user import get_user
 
 
@@ -21,26 +21,26 @@ def create_app():
 
     app.add_url_rule("/", view_func=views.home_page, methods=["GET", "POST"])
 
-    app.add_url_rule(
-        "/login", view_func=views.login_page, methods=["GET", "POST"]
-    )
-    app.add_url_rule("/logout", view_func=views.logout_page)
+    # app.add_url_rule(
+    #     "/login", view_func=views.login_page, methods=["GET", "POST"]
+    # )
+    # app.add_url_rule("/logout", view_func=views.logout_page)
+    #
+    # app.add_url_rule(
+    #     "/movies", view_func=views.movies_page, methods=["GET", "POST"]
+    # )
+    # app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
+    # app.add_url_rule(
+    #     "/movies/<int:movie_key>/edit",
+    #     view_func=views.movie_edit_page,
+    #     methods=["GET", "POST"],
+    # )
+    # app.add_url_rule(
+    #     "/new-movie", view_func=views.movie_add_page, methods=["GET", "POST"]
+    # )
 
-    app.add_url_rule(
-        "/movies", view_func=views.movies_page, methods=["GET", "POST"]
-    )
-    app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
-    app.add_url_rule(
-        "/movies/<int:movie_key>/edit",
-        view_func=views.movie_edit_page,
-        methods=["GET", "POST"],
-    )
-    app.add_url_rule(
-        "/new-movie", view_func=views.movie_add_page, methods=["GET", "POST"]
-    )
-
-    lm.init_app(app)
-    lm.login_view = "login_page"
+    # lm.init_app(app)
+    # lm.login_view = "login_page"
 
     db = Database()
     app.config["db"] = db
