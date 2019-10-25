2ff0fd42f6f867cff32cb9eab99e237402831dcc
karayilano <karayilano@itu.edu.tr>
2019-10-25 09:46:34 +0300

    crash fix test

--- server.py
+++ server.py
@@ -3,27 +3,26 @@ import views
 from database import Database
 from movie import Movie
 
-def create_app():
-    app = Flask(__name__)
 
-    app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/movies", view_func=views.movies_page, methods=["GET", "POST"])
-    app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
-    app.add_url_rule("/new-movie", view_func=views.movie_add_page, methods=["GET", "POST"])
-    app.add_url_rule(
-        "/movies/<int:movie_key>/edit",
-        view_func=views.movie_edit_page,
-        methods=["GET", "POST"],
-    )
+app = Flask(__name__)
 
-    db = Database()
-    db.add_movie(Movie("Slaughterhouse-Five", year=1972))
-    db.add_movie(Movie("The Shining"))
-    app.config["db"] = db
+app.add_url_rule("/", view_func=views.home_page)
+app.add_url_rule("/movies", view_func=views.movies_page, methods=["GET", "POST"])
+app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
+app.add_url_rule("/new-movie", view_func=views.movie_add_page, methods=["GET", "POST"])
+app.add_url_rule(
+    "/movies/<int:movie_key>/edit",
+    view_func=views.movie_edit_page,
+    methods=["GET", "POST"],
+)
 
-    return app
+db = Database()
+db.add_movie(Movie("Slaughterhouse-Five", year=1972))
+db.add_movie(Movie("The Shining"))
+app.config["db"] = db
+
+
 
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
